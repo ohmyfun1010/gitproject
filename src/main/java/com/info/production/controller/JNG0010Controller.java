3a3a684601f8be8ef2841de29237f82d25b5fa91
ohjiseok <ohmyfun@naver.com>
2024-02-03 13:50:49 +0900

    Revert "Update JNG0010Controller.java"
    This reverts commit 3b082e25e275ccde19509287f2e597ff3ab9585f.

--- src/main/java/com/info/production/controller/JNG0010Controller.java
+++ src/main/java/com/info/production/controller/JNG0010Controller.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JNG0010Controller {
-
-	@GetMapping("/JNF0010/0002")
-	public String getData() {
-		return "data";
-	}
 	
 	@GetMapping("/JNF0010/0001")
 	public String main() {
