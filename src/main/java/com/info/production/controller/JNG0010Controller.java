3b082e25e275ccde19509287f2e597ff3ab9585f
OhJiseok <62956948+ohmyfun1010@users.noreply.github.com>
2024-02-03 13:00:58 +0900

    Update JNG0010Controller.java

--- src/main/java/com/info/production/controller/JNG0010Controller.java
+++ src/main/java/com/info/production/controller/JNG0010Controller.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JNG0010Controller {
+
+	@GetMapping("/JNF0010/0002")
+	public String getData() {
+		return "data";
+	}
 	
 	@GetMapping("/JNF0010/0001")
 	public String main() {
